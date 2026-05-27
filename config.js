0583af3b5d17158cb88ffabd8f078d8ae4427245
Ali Sajid <alisajidd565@gmail.com>
2026-05-27 22:02:34 +0500

    fix: stop leaking API key

---
+++ config.js
@@ -0,0 +1,8 @@
+const CFG = {
+  groqKey: "GROQ_KEY_PLACEHOLDER",
+  adminPass: "",
+  supabaseUrl: "https://rsrfmtcltppmslqmhiqe.supabase.co",
+  supabaseAnon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzcmZtdGNsdHBwbXNscW1oaXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3ODM4MTEsImV4cCI6MjA5NDM1OTgxMX0.CDSsgnKGv-XMvKAEC638vars9Aua4idabC_dy0pDqhs",
+  cloudinaryCloud: "dinyg5ryl",
+  cloudinaryPreset: "qazi_league",
+};
