e55d1556be86455a5a347980b91df35bb13585c5
alisajidd565-blip <alisajidd565@gmail.com>
2026-05-28 12:53:34 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CFG = {
-  groqKey: "GROQ_KEY_PLACEHOLDER",
+  groqKey: "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",
   adminPass: "",
   supabaseUrl: "https://rsrfmtcltppmslqmhiqe.supabase.co",
   supabaseAnon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzcmZtdGNsdHBwbXNscW1oaXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3ODM4MTEsImV4cCI6MjA5NDM1OTgxMX0.CDSsgnKGv-XMvKAEC638vars9Aua4idabC_dy0pDqhs",
