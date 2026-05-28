44c76b8ad1c01c4b2935f50cd6f62eecbb3e546e
alisajidd565-blip <alisajidd565@gmail.com>
2026-05-28 13:03:51 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CFG = {
-  groqKey: "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",
+  groqKey: "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",
   adminPass: "",
   supabaseUrl: "https://rsrfmtcltppmslqmhiqe.supabase.co",
   supabaseAnon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzcmZtdGNsdHBwbXNscW1oaXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3ODM4MTEsImV4cCI6MjA5NDM1OTgxMX0.CDSsgnKGv-XMvKAEC638vars9Aua4idabC_dy0pDqhs",
